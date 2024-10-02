4e7f8f4487ec332b607f43eba9de96c2a41773e8
Meiirim <Meiirim>
2024-10-02 16:23:13 +0500

    Resized FlavioShip, added a player movement and Player class

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ pygame.init()
 screen = pygame.display.set_mode((960, 600))
 pygame.display.set_caption("Fundamental Force")
 
-image = pygame.image.load('FlavioShip.png')
+class Player:
+    def __init__(self, x, y, speed):
+        self.sprite = pygame.image.load('FlavioShip.png')
+
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+player = Player(0, 0, 1)
 
 running = True
 while running:
@@ -12,7 +20,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player.y >= 0:
+        player.y -= player.speed
+    elif keys[pygame.K_DOWN] and player.y <= 539:
+        player.y += player.speed
+    elif keys[pygame.K_RIGHT] and player.x <= 901:
+        player.x += player.speed
+    elif keys[pygame.K_LEFT] and player.x >= 0:
+        player.x -= player.speed
+
     screen.fill((0, 0, 0))
-    screen.blit(image, (0, 0))
+    screen.blit(player.sprite, (player.x, player.y))
 
     pygame.display.update()
