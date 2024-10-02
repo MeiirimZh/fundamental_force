ca35a05400e258c687e7c3196dd54a11bd6c89b7
Meiirim <Meiirim>
2024-10-02 15:45:19 +0500

    Added FlavioShip image and output it on screen

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((960, 600))
+pygame.display.set_caption("Fundamental Force")
+
+image = pygame.image.load('FlavioShip.png')
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 0, 0))
+    screen.blit(image, (0, 0))
+
     pygame.display.update()
